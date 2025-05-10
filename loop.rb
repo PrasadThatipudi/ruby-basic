baaaaee38a67a99ccce6b68e432ac691b5e41105
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-05-10 22:37:30 +0530

    Times loop

--- loop.rb
+++ loop.rb
@@ -1,25 +1,13 @@
-# 1..10 --> 10 is inclusive
-for i in 1..10
-  puts "i is #{i}"
+# times loop
+
+5.times do
+  puts "Hello world"
 end
 
-puts "----END OF LOOP----"
+puts "----END OF LOOP----\n\n"
 
-# 1...10 --> 10 is exclusive
-for i in 1...10
-  puts "i is #{i}"
+5.times do |number|
+  puts "number is #{number}"
 end
 
-puts "----END OF LOOP----"
-
-for i in "a".."z"
-  puts "i is #{i}"
-end
-
-puts "----END OF LOOP----"
-
-for i in "aa".."zz"
-  puts "i is #{i}"
-end
-
-puts "----END OF LOOP----"
+puts "----END OF LOOP----\n"
