81657739007ce74d62809d4a83454f814f8c6f57
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-05-10 22:46:52 +0530

    upto and downto loops

--- loop.rb
+++ loop.rb
@@ -1,13 +1,41 @@
-# times loop
+# upto loop
 
-5.times do
-  puts "Hello world"
+5.upto(10) do 
+  puts "hello world"
 end
 
-puts "----END OF LOOP----\n\n"
+puts "----END OF LOOP----"
 
-5.times do |number|
+5.upto(10) do |number|
   puts "number is #{number}"
 end
 
-puts "----END OF LOOP----\n"
+puts "----END OF LOOP----"
+
+5.upto(10) {
+  |number|
+  puts "number is #{number}"
+}
+
+puts "----END OF LOOP----"
+
+# downto loop
+
+10.downto(5) do 
+  puts "hello world"
+end
+
+puts "----END OF LOOP----"
+
+10.downto(5) do |number|
+  puts "number is #{number}"
+end
+
+puts "----END OF LOOP----"
+
+10.downto(5) {
+  |number|
+  puts "number is #{number}"
+}
+
+puts "----END OF LOOP----"
