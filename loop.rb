e623b7074797615bab112aa5f5c86d0dea88a43c
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-05-10 22:33:47 +0530

    Range object and for loop

--- loop.rb
+++ loop.rb
@@ -1,5 +1,25 @@
-puts "Are we there yet?"
-
-until gets.chomp == "yes" do
-  puts "Are we there yet?"
+# 1..10 --> 10 is inclusive
+for i in 1..10
+  puts "i is #{i}"
 end
+
+puts "----END OF LOOP----"
+
+# 1...10 --> 10 is exclusive
+for i in 1...10
+  puts "i is #{i}"
+end
+
+puts "----END OF LOOP----"
+
+for i in "a".."z"
+  puts "i is #{i}"
+end
+
+puts "----END OF LOOP----"
+
+for i in "aa".."zz"
+  puts "i is #{i}"
+end
+
+puts "----END OF LOOP----"
